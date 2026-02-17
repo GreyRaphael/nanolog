c81f2b85ff89d7aeffbe3c126dc4b7b8c4be2ec1
KTF41_gewei <grey@pku.edu.cn>
2026-02-17 13:34:43 +0800

    add string example

--- spsc_test.cpp
+++ spsc_test.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <print>
+#include <string>
 #include <thread>
 
 void test_heap() {
@@ -45,6 +46,69 @@ void test_heap() {
                 h->msg_type = 1;  // 设置自定义消息类型
                 int* data_ptr = reinterpret_cast<int*>(h + 1);
                 *data_ptr = i;
+            })) {
+                // 队列满了，重试
+                std::this_thread::yield();
+            }
+        }
+        std::println("producer{} send {}", std::this_thread::get_id(), test_count);
+    });
+
+    // 4. 等待线程结束
+    producer.join();
+    consumer.join();
+
+    std::println("pass!");
+}
+
+void test_heap_str() {
+    auto queue = std::make_shared<SPSCVarQueueHeap>(64 * 1024);
+    auto test_count = 1e3;
+
+    // 2. 启动消费者线程
+    std::thread consumer([&] {
+        int expected_val = 0;
+        int received_count = 0;
+
+        while (received_count < test_count) {
+            bool success = queue->tryPop([&](auto* h) {
+                // 获取消息头之后的数据指针
+                auto data_ptr = reinterpret_cast<char*>(h + 1);
+                // h->size - sizeof(SPSCVarQueueHeap::MsgHeader 这一个非常关键
+                auto received_str = std::string(data_ptr, h->size - sizeof(SPSCVarQueueHeap::MsgHeader));
+                auto expected_str = std::format("hello{}", expected_val);
+
+                // 校验数据准确性
+                if (received_str != expected_str) {
+                    std::println("error: expected=\"{}\", recv=\"{}\"", expected_str, received_str);
+                    std::println("Sizes -> expected: {}, recv: {}", expected_str.size(), received_str.size());
+                    for (unsigned char c : received_str) {
+                        std::print("{:02x} ", c);
+                    }
+                    std::println("");
+                }
+
+                expected_val++;
+                received_count++;
+            });
+
+            if (!success) {
+                // 队列为空，忙等或短暂睡眠以降低 CPU 占用
+                std::this_thread::yield();
+            }
+        }
+        std::println("consumer{} recv {}", std::this_thread::get_id(), received_count);
+    });
+
+    // 3. 启动生产者线程
+    std::thread producer([&] {
+        for (int i = 0; i < test_count; ++i) {
+            auto s = std::format("hello{}", i);
+            while (!queue->tryPush(s.size(), [&](auto* h) {
+                h->msg_type = 2;
+                // 非常关键写入size
+                h->size = s.size();  // 确保 size 被正确记录
+                std::memcpy(h + 1, s.data(), s.size());
             })) {
                 // 队列满了，重试
                 std::this_thread::yield();
@@ -214,8 +278,9 @@ void test_stack2heap_shared() {
 int main() {
     // // SPSC Queue on Heap
     // test_heap();
+    test_heap_str();
     // // SPSC Queue on Stack
     // test_stack();
     // test_stack2heap_unique();
-    test_stack2heap_shared();
+    // test_stack2heap_shared();
 }
